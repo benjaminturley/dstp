83a29415a9c57e57484ec639f4d07514ddab687d
benjaminturley <bjam.turley@gmail.com>
2014-10-31 15:00:23 -0700

    finshed levels 1-10

---
+++ Don't Shoot the Puppy/Assets/scripts/TriggerBlack.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBlack : MonoBehaviour 
+{
+	public GameObject panel;
+
+	public void createBlind()
+	{
+		panel.SetActive (true);
+	}
+}
